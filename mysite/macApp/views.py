3eb272625b812de6e41cb6b086012f1c83372e17
Chelsi-create <chelsiworld@gmail.com>
2021-02-07 21:53:57 +0530

    Final Commit

--- mysite/macApp/views.py
+++ mysite/macApp/views.py
@@ -37,55 +37,54 @@ def index(request):
     return render(request, 'index.html')
 
 
-import os
-import json
+# import os
+# import json
 
-import h5py
-import numpy as np
-import pickle as pk
-from PIL import Image
+# import h5py
+# import numpy as np
+# import pickle as pk
+# from PIL import Image
 
-# keras imports
-from keras.models import load_model
-from keras.preprocessing import img_to_array, load_img
-from keras.applications.vgg16 import VGG16, preprocess_input
-from keras.preprocessing import image
-from keras.models import Model
-from keras import backend as K
-import tensorflow as tf
+# # keras imports
+# from keras.models import load_model
+# from keras.preprocessing import img_to_array, load_img
+# from keras.applications.vgg16 import VGG16, preprocess_input
+# from keras.preprocessing import image
+# from keras.models import Model
+# from keras import backend as K
+# import tensorflow as tf
 
-def prepare_img_224(img_path):
-    img = load_img(img_path, target_size=(224, 224))
-    x = img_to_array(img)
-    x = np.expand_dims(x, axis = 0)
-    x = preprocess_input(x)
-    return x
-
-
-global graph
-graph = tf.get_default_graph()
+# def prepare_img_224(img_path):
+#     img = load_img(img_path, target_size=(224, 224))
+#     x = img_to_array(img)
+#     x = np.expand_dims(x, axis = 0)
+#     x = preprocess_input(x)
+#     return x
 
 
-CLASS_INDEX_PATH = 'static/imagenet_class_index.json'
+# global graph
+# graph = tf.get_default_graph()
 
-def get_predictions(preds, top = 5):
 
-    global CLASS_INDEX
-    CLASS_INDEX = json.load(open(CLASS_INDEX_PATH))
+# CLASS_INDEX_PATH = 'static/imagenet_class_index.json'
 
-    results = []
-    for pred in preds:
-        top_indices = pred.argsort()[-top:][::-1]
-        result = [tuple(CLASS_INDEX[str(i)]) + (pred[i],) for i in top_indices]
-        result.sort(key=lambda x: x[2], reverse = True)
-        results.append(result)
-    return results
+# def get_predictions(preds, top = 5):
 
-def graph_color_map(img_224):
-    color_map = load_model('static/')
-    print("Color Mappings.....")
-    out = color_map.predict(img_224)
-    top = get_predictions(out, top=5)
-    for j in top[0]:
-        
+#     global CLASS_INDEX
+#     CLASS_INDEX = json.load(open(CLASS_INDEX_PATH))
+
+#     results = []
+#     for pred in preds:
+#         top_indices = pred.argsort()[-top:][::-1]
+#         result = [tuple(CLASS_INDEX[str(i)]) + (pred[i],) for i in top_indices]
+#         result.sort(key=lambda x: x[2], reverse = True)
+#         results.append(result)
+#     return results
+
+# def graph_color_map(img_224):
+#     color_map = load_model('static/')
+#     print("Color Mappings.....")
+#     out = color_map.predict(img_224)
+
+
 
